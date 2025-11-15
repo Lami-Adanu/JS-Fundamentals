b7b4f86422ecf5ded7f8ae64e33e2f2ecee24124
Lami Adanu <lami.adanu1@gmail.com>
2025-11-15 16:57:18 +0100

    A script that prints a Square

---
+++ 8-square.js
@@ -0,0 +1,10 @@
+const { argv } = require("node:process");
+const num = parseInt(argv[2]);
+
+if (Number.isNaN(num)) {
+    console.log("Missing size");
+}else{
+    for (let i = 1; i <= num; i++) {
+        console.log("X".repeat(num));
+    }
+}
