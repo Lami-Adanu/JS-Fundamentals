88dda48873a3455efd420d6010ee7842fe69c0d8
Lami Adanu <lami.adanu1@gmail.com>
2025-11-15 16:24:49 +0100

    An Integer

---
+++ 5-to_integer.js
@@ -0,0 +1,8 @@
+const { argv } = require("node:process");
+const num = parseInt(argv[2]);
+
+if(Number.isNaN(num)) {
+    console.log("Not a number")
+}else{
+    console.log("My number: ${num}");
+}
