6dbbd56bf0ae03855856d4d6f2484526c7869d15
Lami Adanu <lami.adanu1@gmail.com>
2025-11-16 00:23:53 +0100

    Computed and printed factorial

---
+++ 10-factorial.js
@@ -0,0 +1,10 @@
+const { argv } = require("node:process");
+const num = parseInt(argv[2]);
+
+function factorial(n) {
+    if(Number.isNaN(n) || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+console.log(factorial(num));
